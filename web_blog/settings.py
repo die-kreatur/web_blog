e3a7601f795385a0dddffdd5fb96304d46201875
die-kreatur <parusmajor@list.ru>
2021-09-06 21:37:43 +0300

    Create registration form

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -96,5 +97,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'web_blog', 'static')
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
