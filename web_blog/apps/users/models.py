fba52f1d34a3dbb951d2c93fa4c9517099ecfc9d
die-kreatur <parusmajor@list.ru>
2021-09-08 21:54:46 +0300

    Add option to update profile and create post

--- web_blog/apps/users/models.py
+++ web_blog/apps/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+        if img.height > 200 and img.width > 200:
+            size = (200, 200)
+            img.thumbnail(size)
+            img.save(self.image.path)
