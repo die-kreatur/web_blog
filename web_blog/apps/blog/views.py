6261fe97b03a60f00306d22a26ab596720d0deee
die-kreatur <parusmajor@list.ru>
2021-09-09 14:56:56 +0300

    Provide functions to update and delete posts

--- web_blog/apps/blog/views.py
+++ web_blog/apps/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import (
     ListView, DetailView,
-    CreateView)
+    CreateView, UpdateView,
+    DeleteView)
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, UserPassesTestMixin)
 from .models import Post
 
 def home(request):
@@ -21,7 +24,7 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
 
@@ -29,6 +32,28 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+        
 
 def about(request):
     return render(request, 'blog/about.html')
