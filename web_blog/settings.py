3406d97e53b578160a7e460ad5638aa1b2bd5520
die-kreatur <parusmajor@list.ru>
2021-09-11 00:38:20 +0300

    Update password reset views

--- web_blog/settings.py
+++ web_blog/settings.py
@@ -113,4 +113,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('email_user')
-EMAIL_HOST_PASSWORD = os.environ.get('email_password')
+EMAIL_HOST_PASSWORD = os.environ.get('email_pass')
