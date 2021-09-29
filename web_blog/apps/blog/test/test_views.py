f1a6f6452bce29aaa9e564b3dac7d67b9c547679
die-kreatur <parusmajor@list.ru>
2021-09-29 16:31:04 +0300

    Add TestLatestPostView and fix bug

--- web_blog/apps/blog/test/test_views.py
+++ web_blog/apps/blog/test/test_views.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, RequestFactory, Client
+from django.test import TestCase, RequestFactory, Client, client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from .. views import *
@@ -24,3 +24,13 @@ class TestAboutView(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blog/about.html')
+
+
+class TestLatestPostView(TestCase):
+    """Test LatestPostView"""
+    def test_get_latest_posts(self):
+        client = Client()
+        response = client.get(reverse('latest-posts'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/latest_posts.html')
